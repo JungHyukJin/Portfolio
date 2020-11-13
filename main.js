9ac014272e6eb69abca7bc792a390e022a20058a
JungHyukJin <hyukjin4976@naver.com>
2020-11-13 17:36:16 +0900

    JS: filter

--- main.js
+++ main.js
@@ -50,6 +50,25 @@ arrowUp.addEventListener('click', () => {
   scrollIntoView('#home');
 });
 
+// Projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  if (filter == null) {
+    return;
+  }
+
+  projects.forEach((project) => {
+    if (filter === '*' || filter === project.dataset.type) {
+      project.classList.remove('invisible');
+    } else {
+      project.classList.add('invisible');
+    }
+  });
+});
+
 // Util function
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
