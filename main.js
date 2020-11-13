a1b5e5827dcfd4e0a4efd19c125c0e9640d21e52
JungHyukJin <hyukjin4976@naver.com>
2020-11-13 16:17:52 +0900

    JS: home fade to transparent

--- main.js
+++ main.js
@@ -28,6 +28,14 @@ homeContactBtn.addEventListener('click', () => {
   scrollIntoView('#contact');
 });
 
+// mark home slowly fade to transparent as the window scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+// Util function
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: 'smooth' });
