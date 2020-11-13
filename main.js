26884d0505b41e3b010b64ce4e1e6fa888868407
JungHyukJin <hyukjin4976@naver.com>
2020-11-13 16:44:08 +0900

    JS: arrow-up

--- main.js
+++ main.js
@@ -35,6 +35,21 @@ document.addEventListener('scroll', () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
+// show "arrow up" button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.classList.add('visible');
+  } else {
+    arrowUp.classList.remove('visible');
+  }
+});
+
+// Handle click on the "arrow up" button
+arrowUp.addEventListener('click', () => {
+  scrollIntoView('#home');
+});
+
 // Util function
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
