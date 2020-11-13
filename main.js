ebc951fa6bcce0e9a9259ce05ef5fa5fde6e98a3
JungHyukJin <hyukjin4976@naver.com>
2020-11-13 15:14:32 +0900

    JS: navbar

---
+++ main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// navbar fixed on the top(transparent), change color when it's scrolled down
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add('navbar--dark');
+  } else {
+    navbar.classList.remove('navbar--dark');
+  }
+});
